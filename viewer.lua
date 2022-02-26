23ab3bde855f140052d46e2152c40fb24c353d4b
Tony Park <tony@park-hagiwara.net>
2022-02-26 15:53:48 +0000

    Added load of graphic file and display

--- viewer.lua
+++ viewer.lua
@@ -1,7 +1,7 @@
 viewer = {}
 
 function viewer:load()
-  
+    viewer.graphic = love.graphics.newImage("main-view.png")
 end
 
 function viewer:update(dt)
@@ -9,5 +9,5 @@ function viewer:update(dt)
 end
 
 function viewer:draw()
-  
+   love.graphics.draw(viewer.graphic, 0, 0, 0, 2, 2) 
 end
