1ea5acbbca105465ee09930accc2ce9a70c66e89
Tony Park <tony@park-hagiwara.net>
2022-04-01 20:15:50 +0100

    Load and save implemented, some bugs fixed

--- story-editor/editor.lua
+++ story-editor/editor.lua
@@ -2,7 +2,9 @@
 
 local suit = require("suit")
 require("utils")
-
+--local json = require("json")
+--local bitser = require("bitser")
+local smallfolk = require 'smallfolk'
 
 editor = {}
 
@@ -15,8 +17,8 @@ local data =
             -- alternate story text at each node
             {
                 id = 100,
-                text =      { {text={""}} }, 
-                reqs =      { {text={""}} } 
+                text =      { text={""} }, 
+                reqs =      { text={""} } 
             }
         },
         
@@ -25,15 +27,16 @@ local data =
             -- multiple options at each node
             {
                 id = 101,
-                text =      { {text={""}} },
-                reqs =      { {text={""}} },
-                results =   { {text={""}} }
+                text =      { text={""} },
+                reqs =      { text={""} },
+                results =   { text={""} }
             }
         }
     }
 }
 
-
+local filename = "...none..."
+local editable_filename = { text={""} }
 
 local scroll_offset = 0
 local story_node = 1
@@ -50,6 +53,104 @@ end
 function editor:update()
     -- all the UI is defined in love.update or functions that are called from it
 	
+    local function load_file()
+        if filename == '...none...' then return end
+        local load_string
+        local length
+        load_string, length = love.filesystem.read(filename)
+        if length > 0 then
+            local load_data = smallfolk.loads(load_string)
+            -- validate??
+            data = deep_copy(load_data)
+            story_node = 1
+            story_alt = 1
+            -- need to update the id register too
+            mode = 'normal'
+        end
+    end
+        
+    local function save_file() 
+        if filename == '...none...' then mode = 'saveas' return end
+        local save_data = deep_copy_with_ignore(data, {'cursor','cursorline','select','text_draw_offset'})
+--        local save_string = json.encode(save_data)
+--        local new_save_data = json.decode(save_string)
+        local save_string = smallfolk.dumps(save_data)
+        local new_save_data = smallfolk.loads(save_string)
+        
+        local success
+        local message
+        success, message = love.filesystem.write(filename, save_string)
+        if not success then 
+            print("Save failed: "..message) 
+        else
+            print("Saved")
+        end
+        mode = 'normal'
+    end
+
+    local function load_screen()
+        -- display a list of files that could be loaded
+        -- allow a file to be selected
+        -- have a cancel button also
+        
+        local function lload()
+            -- validate the filename
+            -- copy it to 'filename' variable
+            filename = editable_filename.text[1]
+            load_file()
+        end
+        
+        -- or for now...
+        suit.layout:reset(25,25,25)
+        suit.Label("filename",suit.layout:col(100,40))
+        suit.Input(editable_filename, suit.layout:col(600,40))
+        
+        -- load and cancel buttons
+        suit.layout:reset(25,870,25)
+        if suit.Button("Load", suit.layout:row(150,70)).hit then lload() end
+        if suit.Button("Cancel", suit.layout:row(150,70)).hit then mode = 'normal' end
+        
+    end
+    
+    local function save_screen()
+        -- display a list of filenames that could be used (?)
+        -- allow typing of a new filename
+        
+        local function save()
+            -- validate the filename
+            -- copy it to 'filename' variable
+            filename = editable_filename.text[1]
+            save_file()
+        end
+        
+        
+        suit.layout:reset(25,25,25)
+        suit.Label("filename",suit.layout:col(100,40))
+        suit.Input(editable_filename, suit.layout:col(600,40))
+        
+        
+        -- save and cancel buttons
+        suit.layout:reset(25,870,25)
+        if suit.Button("Save", suit.layout:row(150,70)).hit then save() end
+        if suit.Button("Cancel", suit.layout:row(150,70)).hit then mode = 'normal' end
+    end
+    
+    if mode == 'load' then load_screen() return end
+    if mode == 'saveas' then save_screen() return end
+    
+    -- NORMAL MODE
+	-- load and save buttons
+    suit.layout:reset(25,900,15)
+    if suit.Button("Load", suit.layout:row(150,40)).hit then mode = 'load' end
+    if suit.Button("Save", suit.layout:row(150,40)).hit then save_file() end
+    if suit.Button("Save As", suit.layout:row(150,40)).hit then mode = 'saveas' end
+    suit.layout:padding(0)
+    suit.layout:up(0, 55)
+    suit.layout:right(180, 0)        -- I don't understand why these values work but whatever
+    suit.Label("filename",suit.layout:col(100,40))
+    suit.Label(filename, {align='left'}, suit.layout:col(600,40))
+    suit.layout:padding(25)
+    
     local function new_id()
         id_register = id_register + 1
         return id_register
@@ -60,9 +161,6 @@ function editor:update()
     local lw = 80                   -- label width
     
     -- STORY
-	-- the layout will grow down and to the right from this point
-	suit.layout:reset(25,25,25)
-
     local function new_alt_story() 
         table.insert(story, 
             {
@@ -96,14 +194,17 @@ function editor:update()
                 local node = check_status(v2.results.text[1], 'node')
                 if tonumber(node)==story_node then 
                     story_node=k 
+                    story_alt=1
                     return 
                 end
             end
         end
-        
+   
     end
     
-
+	-- the layout will grow down and to the right from this point
+	suit.layout:reset(25,25,25)
+    
 	-- put an input widget at the layout origin, with a cell size of 200 by 30 pixels
 	suit.Label("Story", suit.layout:row(150, 25))
     suit.layout:padding(0)
@@ -134,7 +235,7 @@ function editor:update()
     
     
     -- OPTIONS
-	suit.layout:reset(25,350,25)
+    local options_to_display=4
     
     local function new_node(k)
         table.insert(data,
@@ -144,8 +245,8 @@ function editor:update()
                     -- alternate story text at each node
                     {
                         id = new_id(),
-                        text =      { {text={""}} }, 
-                        reqs =      { {text={""}} } 
+                        text =      { text={""} }, 
+                        reqs =      { text={""} } 
                     }
                 },
                 
@@ -154,9 +255,9 @@ function editor:update()
                     -- multiple options at each node
                     {
                         id = new_id(),
-                        text =      { {text={""}} },
-                        reqs =      { {text={""}} },
-                        results =   { {text={""}} }
+                        text =      { text={""} },
+                        reqs =      { text={""} },
+                        results =   { text={""} }
                     }
                 }
             })
@@ -164,6 +265,7 @@ function editor:update()
         local results = options[k].results.text[1]
         options[k].results.text[1] = "node="..#data..";"..results
         story_node = #data
+        story_alt = 1
     end
     
     local function node(k)
@@ -181,7 +283,7 @@ function editor:update()
             table.remove(options, which)
             
             -- fix scroll offset
-            if scroll_offset > #options-5 then scroll_offset = #options-5 end
+            if scroll_offset > #options-options_to_display then scroll_offset = #options-options_to_display end
             if scroll_offset < 0 then scroll_offset = 0 end
         end
     end
@@ -200,7 +302,7 @@ function editor:update()
             })
             
         -- set the scroll offset so the new option is visible
-        scroll_offset = #options-5
+        scroll_offset = #options-options_to_display
         if scroll_offset < 0 then scroll_offset = 0 end
     end
     
@@ -224,7 +326,7 @@ function editor:update()
                 scroll_offset = scroll_offset - 1
             end
         else
-            if scroll_offset < #options-5 then
+            if scroll_offset < #options-options_to_display then
                 scroll_offset = scroll_offset + 1
             end
         end
@@ -234,6 +336,8 @@ function editor:update()
             
     
     local tw = 700-50-50-25-25-80   -- text width
+    
+	suit.layout:reset(25,350,25)
     
     local function entry(k)
         suit.Label("opt"..k,suit.layout:col(lw,35))
@@ -247,24 +351,27 @@ function editor:update()
         suit.Input(options[k].results, suit.layout:col(tw,35))
         suit.layout:up(tw,70)
         suit.layout:padding(25)
-        if suit.Button("Del", {id="B"..options[k].id}, suit.layout:col(50,52)).hit then delete(k) end
+        
+        -- check out the horrible construct below
+        -- we're using 'else' to make sure we don;t process the other buttons if one actually gets hit
+        if suit.Button("Del", {id="B"..options[k].id}, suit.layout:col(50,52)).hit then delete(k) else
         suit.layout:padding(0)
-        if suit.Button("Node", {id="N"..options[k].id}, suit.layout:row(50,53)).hit then node(k) end
+        if suit.Button("Node", {id="N"..options[k].id}, suit.layout:row(50,53)).hit then node(k) else
         suit.layout:up(50,52)
         suit.layout:padding(25)
-        if suit.Button("U", {id="U"..options[k].id}, suit.layout:col(50,52)).hit then up(k) end
+        if suit.Button("U", {id="U"..options[k].id}, suit.layout:col(50,52)).hit then up(k) else
         suit.layout:padding(0)
-        if suit.Button("D", {id="D"..options[k].id}, suit.layout:row(50,53)).hit then down(k) end
+        if suit.Button("D", {id="D"..options[k].id}, suit.layout:row(50,53)).hit then down(k) end end end end
     end
 
     -- this must be a while loop not a for loop, because the BIN button 
     -- can change the length of value of #options
     local k=1
-    while k <= math.min(#options, 5) do -- we cannot calculate this prior to the loop!!
+    while k <= math.min(#options, options_to_display) do -- we cannot calculate this prior to the loop!!
         suit.layout:reset(25,350+(130*(k-1)),25)
         if k == 1 then
             if suit.Button("New Option", suit.layout:row(150,70)).hit then new_option() end
-        elseif k == 2 and #options > 5 then
+        elseif k == 2 and #options > options_to_display then
             suit.layout:col(50,70)
             suit.layout:padding(0)
             if suit.Button("SU", suit.layout:col(50,52)).hit then scroll('up') end
@@ -278,6 +385,20 @@ function editor:update()
         entry(k+scroll_offset)
         k = k + 1
     end
+--[[
+    -- STORY MAP
+    local function navigate()
+    end
+        
+	suit.layout:reset(1000,25,25)
+    
+    -- data[1] is always the top level
+    for alt = 1, #data[1].story do
+        if suit.Button("1."..alt, suit.layout:col(50,50)).hit then navigate("1."..alt) end
+    end
+        
+    -- for all other nodes, need to work out what the deepest level is
+--]]    
 
 end
 
--- story-editor/utils.lua
+++ story-editor/utils.lua
@@ -15,3 +15,37 @@ function check_status(status_string, status)
     return nil
 end
         
+-- basic deep copy
+-- does not handle metatables 
+-- does not handle recursive tables - class tables are recursive
+function deep_copy(item)
+    if type(item) ~= 'table' then 
+        return item 
+    end
+    
+    local copy = {}
+    for k,v in pairs(item) do
+        copy[k] = deep_copy(v)
+    end
+    
+    return copy
+end
+
+function deep_copy_with_ignore(item, ignore)
+    if type(item) ~= 'table' then 
+        return item 
+    end
+    
+    local copy = {}
+    for k,v in pairs(item) do
+        local do_copy = true
+        for _,i in ipairs(ignore) do
+            if k == i then
+                do_copy = false
+            end
+        end
+        if do_copy then copy[k] = deep_copy_with_ignore(v, ignore) end
+    end
+    
+    return copy
+end
